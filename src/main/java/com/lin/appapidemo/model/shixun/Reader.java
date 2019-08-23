08eb964b8ab278f2bd56103e8c7cc0999f7e9701
tangsk <40078060+tangsk@users.noreply.github.com>
2019-08-23 20:28:22 +0800

    Update Reader.java

--- src/main/java/com/lin/appapidemo/model/shixun/Reader.java
+++ src/main/java/com/lin/appapidemo/model/shixun/Reader.java
@@ -1,12 +1,12 @@
 package com.lin.appapidemo.model.shixun;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.experimental.Accessors;
+        import lombok.Data;
+        import lombok.NoArgsConstructor;
+        import lombok.experimental.Accessors;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
+        import javax.persistence.Column;
+        import javax.persistence.Id;
+        import javax.persistence.Table;
 
 @Data
 @NoArgsConstructor
@@ -37,14 +37,80 @@ public class Reader {
     @Column(name = "phoneNumber")
     private String phoneNumber;
 
-    
 
-    public Reader(String account, String password, String name, String time, int condi) {
+
+    public Reader(String account, String password, String name, String time, int condi,String feedback,String EMailAddr,String phoneNumber) {
         this.account = account;
         this.password = password;
         this.name = name;
         this.time = time;
         this.condi = condi;
-      
+        this.feedback = feedback;
+        this.EMailAddr = EMailAddr;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAccount(){
+        return account;
+    }
+
+    public void setAccount(String account){
+        this.account=account;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password=password;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public String getTime(){
+        return time;
+    }
+
+    public void setTime(String time){
+        this.time=time;
+    }
+
+    public int getCondi(){
+        return condi;
+    }
+
+    public void setCondi(int condi){
+        this.condi=condi;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback() {
+        this.feedback=feedback;
+    }
+
+    public String getEMailAddr() {
+        return EMailAddr;
+    }
+
+    public void setEMailAddr() {
+        this.EMailAddr=EMailAddr;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber() {
+        this.phoneNumber=phoneNumber;
     }
 }
